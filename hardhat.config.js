8c46c52249d44487457d702c5c0f8a74e8f53e50
Antyibvak Ibrahim <tvax@Antyibvaks-MacBook-Pro.local>
2022-06-14 14:41:06 -0400

    my changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
+const metamask_private_key = "0x372e2d9906c37a41896da495ce2711b2dae207c489c766ffd7135cf39a5d5bfb"
+
+
 module.exports = {
   solidity: "0.8.4",
   paths: {
@@ -8,5 +11,11 @@ module.exports = {
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
   },
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/97f53876cccc42dbb5303c6b3707a173",
+      accounts: [metamask_private_key],
+    }
+  }
 };
 
